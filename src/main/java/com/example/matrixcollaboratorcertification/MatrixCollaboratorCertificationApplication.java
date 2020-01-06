b2e083fbdd97b0f7767c7320f9d1ad99ff9ea11b
Benderouach Hamza <hbenderouach@L530561.SQLI>
2020-01-06 01:11:16 +0100

    first commit

---
+++ src/main/java/com/example/matrixcollaboratorcertification/MatrixCollaboratorCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.matrixcollaboratorcertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixCollaboratorCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixCollaboratorCertificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/matrixcollaboratorcertification/MatrixCollaboratorCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matrixcollaboratorcertification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixCollaboratorCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
